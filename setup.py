0fdcba8a031964d97fd5a4c1fb8839f1905b65a2
julianbarg <barg.julian@gmail.com>
2019-06-29 14:26:35 -0400

    Added version number update.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(name='juujian',
-                 version='0.0.7',
+                 version='0.0.8',
                  description='Various web scraping and data science related tools. ',
                  long_description=long_description,
                  long_description_content_type='text/markdown',
